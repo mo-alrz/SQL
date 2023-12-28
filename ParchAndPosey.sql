ddd592fe670fef39bd31cf98c36c8fb2fb6c318b
Mohammad <m.alirezaaee.1986@gmail.com>
2023-12-28 14:29:50 +0100

    Parch and posey

--- ParchAndPosey.sql
+++ ParchAndPosey.sql
@@ -227,7 +227,10 @@ WHERE (name LIKE 'C%' OR name LIKE 'W%')
       AND (primary_poc LIKE '%ana%' or primary_poc LIKE '%Ana%')
       AND primary_poc NOT LIKE '%eana%';
 
---Join (Inner -> Returns rows that appear in both tables, Left, Right, Full outer)
+--Join (Inner -> Returns rows that appear in both tables,
+--      Left -> pulls all the data that exists in both tables, as well as all of the rows from the table in the FROM even if they do not exist in the JOIN statement,
+--      Right -> pulls all the data that exists in both tables, as well as all of the rows from the table in the JOIN even if they do not exist in the FROM statement,
+--      Full outer -> returns all records when there is a match in left (table1) or right (table2) table records.)
 --Try pulling all the data from the accounts table, and all the data from the orders table.
 SELECT a.* , o.*
 FROM accounts a
@@ -372,4 +375,190 @@ ON a.id = o.account_id
 WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016'
 ORDER BY o.occurred_at DESC;
 
+--Union
+--The UNION operator is used to combine the result-set of two or more SELECT statements.
+--  Every SELECT statement within UNION must have the same number of columns
+--  The columns must also have similar data types
+--  The columns in every SELECT statement must also be in the same order
+SELECT column_name(s) FROM table1
+UNION
+SELECT column_name(s) FROM table2;
 
+--The UNION operator selects only distinct values by default. To allow duplicate values, use UNION ALL:
+SELECT column_name(s) FROM table1
+UNION ALL
+SELECT column_name(s) FROM table2;
+
+--Aggregations
+--Find the total amount of poster_qty paper ordered in the orders table.
+SELECT SUM(poster_qty) AS total_poster_sales
+FROM orders;
+
+--Find the total amount of standard_qty paper ordered in the orders table.
+SELECT SUM(standard_qty) AS total_standard_sales
+FROM orders;
+
+--Find the total dollar amount of sales using the total_amt_usd in the orders table.
+SELECT SUM(total_amt_usd) AS total_dollar_sales
+FROM orders;
+
+--Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table.
+SELECT standard_amt_usd + gloss_amt_usd AS total_standard_gloss
+FROM orders;
+
+--Find the standard_amt_usd per unit of standard_qty paper.
+SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS standard_price_per_unit
+FROM orders;
+
+--When was the earliest order ever placed? You only need to return the date.
+SELECT MIN(occurred_at)
+FROM orders;
+
+--Try performing the same query as in question 1 without using an aggregation function.
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1;
+
+--When did the most recent (latest) web_event occur?
+SELECT MAX(occurred_at)
+FROM orders;
+
+--Try to perform the result of the previous query without using an aggregation function.
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at DESC
+LIMIT 1;
+
+--Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type
+--purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales,
+--as well as the average amount.
+SELECT AVG(standard_qty), AVG(gloss_qty), AVG(poster_qty), AVG(standard_amt_usd),AVG(gloss_amt_usd), AVG(poster_amt_usd)
+FROM orders;
+
+--Via the video, you might be interested in how to calculate the MEDIAN. Though this is more advanced than what we have
+--covered so far try finding - what is the MEDIAN total_usd spent on all orders?
+SELECT *
+FROM (SELECT total_amt_usd
+      FROM orders
+      ORDER BY total_amt_usd
+      LIMIT 3457) AS Table1
+ORDER BY total_amt_usd DESC
+LIMIT 2;
+
+--Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY occurred_at
+LIMIT 1;
+
+--Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
+SELECT a.name, SUM(o.total_amt_usd)
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+--Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event?
+--Your query should return only three values - the date, channel, and account name.
+SELECT w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+ORDER BY w.occurred_at DESC
+LIMIT 1;
+
+--Find the total number of times each type of channel from the web_events was used. Your final table should have two
+--columns - the channel and the number of times the channel was used.
+SELECT w.channel, COUNT(*)
+FROM web_events w
+GROUP BY w.channel;
+
+--Who was the primary contact associated with the earliest web_event?
+SELECT a.primary_poc
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+--What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account
+--name and the total usd. Order from smallest dollar amounts to largest.
+SELECT a.name, MIN(o.total_amt_usd) smallest_order
+FROM accounts
+JOIN orders
+ON a.id = o.account_id
+GROUP BY a.name
+ORDER BY smallest_order;
+
+--Find the number of sales reps in each region. Your final table should have two columns - the region and the number of
+--sales_reps. Order from fewest reps to most reps.
+SELECT r.name, COUNT(s.id)
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+GROUP BY r.name
+ORDER BY num_reps;
+
+--For each account, determine the average amount of each type of paper they purchased across their orders. Your result
+--should have four columns - one for the account name and one for the average quantity purchased for each of the paper
+--types for each account.
+SELECT a.name, AVG(poster_qty),AVG(standard_qty),AVG(gloss_qty)
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+--For each account, determine the average amount spent per order on each paper type. Your result should have four columns
+--one for the account name and one for the average amount spent on each paper type.
+SELECT a.name, AVG(gloss_amt_usd),AVG(standard_amt_usd),AVG(poster_amt_usd)
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.name;
+
+--Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table
+--should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table
+--with the highest number of occurrences first.
+SELECT s.name, w.channel, COUNT(*) num_events
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+GROUP BY s.name, w.channel
+ORDER BY num_events DESC;
+
+--Determine the number of times a particular channel was used in the web_events table for each region. Your final table
+--should have three columns - the region name, the channel, and the number of occurrences. Order your table with the
+--highest number of occurrences first.
+SELECT r.name, w.channel, COUNT(*) num_events
+FROM web_events w
+JOIN accounts a
+ON a.id = w.account_id
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+GROUP BY s.name, w.channel
+ORDER BY num_events DESC;
+
+--Use DISTINCT to test if there are any accounts associated with more than one region.
+--The below two queries have the same number of resulting rows (351), so we know that every account is associated with
+--only one region. If each account was associated with more than one region, the first query should have returned more
+--rows than the second query.
+SELECT a.id account_id, a.name account_name, r.id region_id, r.name region_name
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON r.id = s.region_id;
+
+--and
+
+SELECT DISTINCT id, name
+FROM accounts;
+
+--Have any sales reps worked on more than one account?
